86d2a8c6bf01e488762c6fa92dda560b4587efac
joelnithushan <joelnithushan6@gmail.com>
2026-02-15 21:52:27 +0530

    express app and server entry configured

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('./config/env');
+const routes = require('./routes');
+const errorHandler = require('./middlewares/error.middleware');
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors());
+app.options('*', cors());
+
+if (config.env !== 'test') {
+    app.use(morgan(config.env === 'production' ? 'combined' : 'dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+app.use(errorHandler);
+
+module.exports = app;
