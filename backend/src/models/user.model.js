d2509c909606628e792699fb432c47873c3046d7
joelnithushan <joelnithushan6@gmail.com>
2026-02-15 21:52:35 +0530

    user schema created with role support

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+            private: true,
+        },
+        role: {
+            type: String,
+            enum: ['ADMIN', 'EMPLOYER', 'JOB_SEEKER'],
+            default: 'JOB_SEEKER',
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
+    const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
+    return !!user;
+};
+
+userSchema.methods.isPasswordMatch = async function (password) {
+    const user = this;
+    return bcrypt.compare(password, user.password);
+};
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    delete userObject.__v;
+    return userObject;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
