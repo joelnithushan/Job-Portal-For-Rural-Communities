7efa2d95c1923770d2abdce80a4e6104aa7d58e3
joelnithushan <joelnithushan6@gmail.com>
2026-02-15 21:56:08 +0530

    global error handler and response utility configured

---
+++ backend/src/utils/response.js
@@ -0,0 +1,26 @@
+const successResponse = (res, message, data = null, statusCode = 200) => {
+    const response = {
+        success: true,
+        message,
+    };
+    if (data) {
+        response.data = data;
+    }
+    return res.status(statusCode).json(response);
+};
+
+const errorResponse = (res, message, statusCode = 500, errors = null) => {
+    const response = {
+        success: false,
+        message,
+    };
+    if (errors) {
+        response.errors = errors;
+    }
+    return res.status(statusCode).json(response);
+};
+
+module.exports = {
+    successResponse,
+    errorResponse,
+};
