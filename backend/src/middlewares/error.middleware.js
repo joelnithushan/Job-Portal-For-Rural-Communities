92e08ba58c4904766a84999ad4c77f393df8d448
joelnithushan <joelnithushan6@gmail.com>
2026-02-18 11:53:58 +0530

    fix critical bugs in middlewares

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,30 +1,19 @@
 const config = require('../config/env');
-const { errorResponse } = require('../utils/response');
 
 const errorHandler = (err, req, res, next) => {
-    let { statusCode, message } = err;
-    if (!statusCode) statusCode = 500;
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Internal Server Error';
 
     if (config.env === 'production' && !err.isOperational) {
         statusCode = 500;
         message = 'Internal Server Error';
     }
 
-    res.locals.errorMessage = err.message;
-
-    const response = {
-        code: statusCode,
+    return res.status(statusCode).json({
+        success: false,
         message,
         ...(config.env === 'development' && { stack: err.stack }),
-    };
-
-    console.error(err);
-
-    if (config.env === 'development') {
-        return res.status(statusCode).json({ success: false, ...response });
-    }
-
-    return errorResponse(res, message, statusCode);
+    });
 };
 
 module.exports = errorHandler;
--- backend/src/middlewares/validate.middleware.js
+++ backend/src/middlewares/validate.middleware.js
@@ -19,7 +19,6 @@ const validate = (schema) => (req, res, next) => {
 const pick = (object, keys) => {
     return keys.reduce((obj, key) => {
         if (object && Object.prototype.hasOwnProperty.call(object, key)) {
-
             obj[key] = object[key];
         }
         return obj;
